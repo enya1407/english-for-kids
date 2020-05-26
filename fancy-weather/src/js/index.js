e21b280e74e5f9285cbea777604e159bfbbe6013
Enya <enya1407@gmail.com>
2020-05-26 16:51:51 +0300

    feat: add header

---
+++ fancy-weather/src/js/index.js
@@ -0,0 +1,25 @@
+const backgroundSwitch = document.querySelector('.btn-outline-secondary')
+const arrowSpinning = document.querySelector('.bi-arrow-repeat')
+const languageButton = document.querySelector('#languageButton')
+const dropdownMenu = document.querySelector(".dropdown-menu")
+
+backgroundSwitch.addEventListener('click', () => {
+  arrowSpinning.classList.add('spinning')
+  setTimeout(()=>{arrowSpinning.classList.remove('spinning')}, 500) 
+})
+
+dropdownMenu.addEventListener('click', event => {
+  
+    if (event.target.classList.contains("dropdown-item")) {
+      dropdownMenu
+      .querySelectorAll(".dropdown-item")
+      .forEach(el => el.classList.remove("active"));
+      event.target.classList.add("active");
+      languageButton.textContent = event.target.textContent
+    }
+})
+
+
+
+
+
